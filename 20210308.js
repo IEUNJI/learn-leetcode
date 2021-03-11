19d962cc7e1b1592fcd2baf0e4a0b4bf0577a788
linzijun <linzjk@yonyou.com>
2021-03-11 13:14:24 +0800

    完善中位数二分查找和回文串动态规划

--- 20210308.js
+++ 20210308.js
@@ -6,6 +6,46 @@
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
+  if (nums1.length > nums2.length) {
+    var temp = nums1;
+    nums1 = nums2;
+    nums2 = temp;
+  }
+
+  var len1 = nums1.length;
+  var len2 = nums2.length;
+
+  var leftTotal = Math.floor((len1 + len2 + 1) / 2);
+
+  var left = 0;
+  var right = len1;
+
+  while (left < right) {
+    var i = Math.floor((left + right + 1) / 2);
+    var j = leftTotal - i;
+
+    if (nums1[i - 1] > nums2[j]) {
+      right = i - 1;
+    } else {
+      left = i;
+    }
+  }
+
+  var i = left;
+  var j = leftTotal - i;
+
+  var leftMax1 = i - 1 < 0 ? Number.NEGATIVE_INFINITY : nums1[i - 1];
+  var leftMax2 = j - 1 < 0 ? Number.NEGATIVE_INFINITY : nums2[j - 1];
+  var rightMin1 = i + 1 > len1 ? Number.POSITIVE_INFINITY : nums1[i];
+  var rightMin2 = j + 1 > len2 ? Number.POSITIVE_INFINITY : nums2[j];
+
+  if ((len1 + len2) % 2 === 1) {
+    return Math.max(leftMax1, leftMax2);
+  } else {
+    return (Math.max(leftMax1, leftMax2) + Math.min(rightMin1, rightMin2)) / 2;
+  }
+};
+var findMedianSortedArrays1 = function (nums1, nums2) {
   var len1 = nums1.length;
   var len2 = nums2.length;
   var isOdd = (len1 + len2) % 2 === 1;
@@ -53,11 +93,13 @@ var findMedianSortedArrays = function (nums1, nums2) {
   }
 };
 
-// var nums1 = [1, 3], nums2 = [2];
-// var nums1 = [1], nums2 = [2];
-// var nums1 = [1, 2], nums2 = [3, 4];
-// var nums1 = [0, 0], nums2 = [0, 0];
-// var nums1 = [], nums2 = [1];
-// var nums1 = [2], nums2 = [];
+var nums1 = [1, 3], nums2 = [2];
+var nums1 = [1], nums2 = [2];
+var nums1 = [1, 2], nums2 = [3, 4];
+var nums1 = [0, 0], nums2 = [0, 0];
+var nums1 = [], nums2 = [1];
+var nums1 = [2], nums2 = [];
+var nums1 = [1, 4, 5], nums2 = [2, 3, 6];
+var nums1 = [1, 3, 4, 5, 6, 7, 9], nums2 = [2, 8, 10, 11, 12, 13, 14, 15];
 
 console.log(findMedianSortedArrays(nums1, nums2));
--- 20210309.js
+++ 20210309.js
@@ -5,6 +5,39 @@
  * @return {string}
  */
 var longestPalindrome = function (s) {
+  var dp = [];
+  var maxLen = 1;
+  var begin = 0;
+
+  for (var i = 0; i < s.length; i++) {
+    dp[i] = [];
+    for (var j = 0; j < s.length; j++) {
+      dp[i][j] = i === j;
+    }
+  }
+
+  for (var y = 1; y < s.length; y++) {
+    for (var x = 0; x < y; x++) {
+      if (s[x] !== s[y]) {
+        dp[x][y] = false;
+      } else {
+        if (y - x < 3) {
+          dp[x][y] = true;
+        } else {
+          dp[x][y] = dp[x + 1][y - 1];
+        }
+      }
+
+      if (dp[x][y] && y - x + 1 > maxLen) {
+        maxLen = y - x + 1;
+        begin = x;
+      }
+    }
+  }
+  return s.substr(begin, maxLen);
+};
+
+var longestPalindrome1 = function (s) {
   function isPalindrome(s) {
     for (var i = 0; i <= Math.floor((s.length - 1) / 2); i++) {
       if (s[i] === s[s.length - 1 - i]) {
@@ -39,14 +72,14 @@ var longestPalindrome = function (s) {
   return palindrome;
 };
 
-var s = "babad";
-// var s = "cbbd";
-// var s = "a";
-// var s = "ac";
+var s = "babab";
+var s = "cbbd";
+var s = "a";
+var s = "ac";
 
-// var s = "babcdefgfedcbaa";
+var s = "babcdefgfedcbaa";
 
-// var s = "zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir";
-// var s = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
+var s = "zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir";
+var s = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
 
 console.log(longestPalindrome(s));
