e0c127d7c4c865b2b32388bb7fc85b5a7d81f8e9
linzijun <linzjk@yonyou.com>
2021-05-27 13:54:42 +0800

    手写算法题 排序

---
+++ 手写算法题.js
@@ -0,0 +1,155 @@
+/**
+ * 二分查找
+ * 适用场景：在已经排序的数据中查找
+ */
+
+function bsearch(arr, target) {
+  let l = 0; // 左边界
+  let r = arr.length - 1; // 右边界
+  let guess; // 猜想位置
+
+  while (l <= r) {
+    guess = Math.floor((l + r) / 2); // 取左右边界中间的索引
+
+    if (arr[guess] === target) { // 找到
+      return guess;
+    } else if (arr[guess] > target) { // 目标在左侧区间
+      r = guess - 1; // 舍弃右半部分
+    } else if (arr[guess] < target) { // 目标在右侧区间
+      l = guess + 1; // 舍弃左半部分
+    }
+  }
+
+  return -1;
+}
+
+/**
+ * 插入排序 O(N^2)
+ * 抓牌，将新牌插入到有序的牌中
+ * 子问题：将数据插入到有序的数组中
+ */
+
+function insertSort(arr) {
+  // 初始只有第一项为有序的，所以从第二项循环
+  for (let i = 1; i < arr.length; i++) {
+    const current = arr[i]; // 待插入的元素
+    let p = i - 1; // 比较索引，从 i 的前一项开始比较
+    while (p >= 0 && arr[p] > current) { // 待插入元素小，需要向前移
+      arr[p + 1] = arr[p]; // p 向后移动到 p - 1，因为 i 的位置已经被 current 提出，所以可以覆盖
+      p--; // 继续比较下一项
+    }
+    // 待插入元素比 p 位置的大，可以插入到 p + 1 位置
+    arr[p + 1] = current;
+  }
+}
+
+/**
+ * 冒泡排序 O(N^2)
+ * 双循环，两两交换，将大值不断交换到右侧
+ */
+
+function bubbleSort(arr) {
+  // i 代表：第几次内层循环完，i 指向第几大的值
+  // 第一次循环，内层循环将第一大的值浮上来了
+  for (let i = arr.length - 1; i >= 1; i--) {
+    for (let j = 0; j <= i - 1; j++) {
+      if (arr[j] > arr[j + 1]) { // 前面的大于后面的，需要交换
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
+/**
+ * 归并排序
+ * 分治策略，将数组分割成一项的有序数组，再两两合并有序数组
+ */
+
+function mergeSort(arr) {
+  function split(arr, p, r) {
+    if (r - p < 2) return; // 分割只剩一项
+
+    // 向上取整，因为 q 是左侧结束索引 + 1
+    const q = Math.ceil((p + r) / 2);
+
+    split(arr, p, q);
+    split(arr, q, r);
+
+    // 此时递归结束，开始合并
+    merge(arr, p, q, r);
+  }
+
+  // p 左侧数组的起始索引
+  // q 左侧数组的结束索引+1，即右侧数组的起始索引
+  // r 右侧数组的结束索引+1
+  // 这样设计的好处是slice直接截取即可
+  function merge(arr, p, q, r) {
+    const arr1 = arr.slice(p, q);
+    const arr2 = arr.slice(q, r);
+    // 追加哨兵，保证后续比较到末尾时无需判断索引超界限
+    arr1.push(Number.MAX_SAFE_INTEGER);
+    arr2.push(Number.MAX_SAFE_INTEGER);
+
+    for (let k = p, i = 0, j = 0; k < r; k++) {
+      arr[k] = arr1[i] < arr2[j] ? arr1[i++] : arr2[j++];
+    }
+  }
+
+  split(arr, 0, arr.length);
+}
+
+/**
+ * 快速排序
+ * 分治策略，取数组最右侧的一项作为中心点，从头遍历，将大于中心点的抛到后面
+ * 然后将中心点插入到中心，然后两边数组再重复此过程
+ */
+
+function quickSort(arr) {
+  // 启动函数
+  // lo 低位，hi 高位
+  function sort(arr, lo, hi) {
+    if (hi - lo < 2) return; // 只剩一项
+    // 中心点索引
+    const p = part(arr, lo, hi);
+    sort(arr, lo, p);
+    sort(arr, p + 1, hi);
+  }
+
+  // 分治函数
+  // 取最右侧的一项作为中心点，然后两侧索引不断靠近
+  // 将小于中心点的交换到左侧，大于中心点的交换到右侧
+  // 然后将中心点和右侧的第一项交换，返回中心点的索引
+  function part(arr, lo, hi) {
+    const point = arr[hi - 1];
+    let i = lo; // 先指向第一项
+    let j = hi - 1; // 先指向最后一项
+
+    while (i !== j) { // 没相遇
+      if (arr[i] <= point) { // 小于等于中心点，下一个
+        i++;
+      } else { // 大于中心点，将该值抛到后面
+        // 因为j指向中心点，实际交换的是中心点的前一项
+        swap(arr, i, --j);
+      }
+    }
+
+    // 最后j指向的是大于中心点的第一项，和中心点交换
+    swap(arr, j, hi - 1);
+
+    return j;
+  }
+
+  function swap(arr, m, n) {
+    const temp = arr[m];
+    arr[m] = arr[n];
+    arr[n] = temp;
+  }
+
+  sort(arr, 0, arr.length);
+}
+
+const arr = [12, 13, 452, 245, 3124, 324, 76, 234, 54, 45];
+quickSort(arr);
+console.log(arr);
