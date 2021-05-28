2bbb524ca4c224d6a7acdaa8c4ba120099d9c96c
linzijun <linzjk@yonyou.com>
2021-05-28 10:51:30 +0800

    翻转二叉树

---
+++ 20210528-01.js
@@ -0,0 +1,26 @@
+/**
+ * 226. 翻转二叉树
+ */
+
+function TreeNode(val, left, right) {
+  this.val = (val === undefined ? 0 : val)
+  this.left = (left === undefined ? null : left)
+  this.right = (right === undefined ? null : right)
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (!root) return root;
+
+  var temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+};
