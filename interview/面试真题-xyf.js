28a9413f791d7a8e30c53954630bd63bcfa93fa1
林子峻 <linzijun1@jd.com>
2022-01-07 12:35:15 +0800

    find函数

--- interview/面试真题-xyf.js
+++ interview/面试真题-xyf.js
@@ -880,3 +880,54 @@ function transformToArr(obj) {
 
   return res;
 }
+
+/**
+ * 蚂蚁金服 笔试题
+ * 请实现 find 函数，使下列的代码调用正确
+ */
+
+var data = [
+  { userId: 8, title: 'title1' },
+  { userId: 11, title: 'other' },
+  { userId: 15, title: null },
+  { userId: 19, title: 'title2' }
+];
+
+var find = function (origin) {
+  // your code are here...
+
+  const arrayProto = Array.prototype;
+  const finderProto = Object.create(arrayProto);
+
+  finderProto.where = function where(rules) {
+    const nextOrigin = Object.keys(rules).reduce((acc, key) => {
+      return acc.filter(item => {
+        return rules[key].test(item[key]);
+      });
+    }, this);
+
+    return find(nextOrigin);
+  };
+
+  finderProto.orderBy = function orderBy(key, by) {
+    this.sort((a, b) => {
+      const aVal = a[key];
+      const bVal = b[key];
+
+      return by !== 'desc' ? aVal - bVal : bVal - aVal;
+    });
+
+    return this;
+  };
+
+  Object.setPrototypeOf(origin, finderProto);
+
+  return origin;
+};
+
+// 查找 data 中，符合条件的数据，并进行排序
+var result = find(data).where({
+  'title': /\d$/
+}).orderBy('userId', 'desc');
+
+console.log(result); // [ { userId: 19, title: 'title2' }, { userId: 8, title: 'title1' } ]
